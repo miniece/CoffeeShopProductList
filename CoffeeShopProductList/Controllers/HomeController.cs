0217b663273a8989956044a926a148895a37d488
miniece <mvelinda@icloud.com>
2022-10-31 00:50:32 -0400

    idk bout this one dawg

---
+++ CoffeeShopProductList/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetCore;
+using CoffeeShopProductList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CoffeeShopProductList.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductContext db = new ProductContext();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult ProductList()
+        {
+            List<Product> products = db.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopProductList/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoffeeShopProductList.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Category { get; set; }
+    }
+}
